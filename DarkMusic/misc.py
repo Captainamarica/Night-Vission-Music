1dce4d4075051bf66b26fbedbb9435dc921f4bef
Navanjana <104069319+Captainamarica@users.noreply.github.com>
2022-05-22 14:00:34 +0530

    Update misc.py

--- DarkMusic/misc.py
+++ DarkMusic/misc.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 by TEAM-DARK-MUSIC@Github, < https://github.com/TEAM-DARK-MUSIC >.
+# Copyright (C) 2022 by TEAM-DARK-MUSIC@Github, < https://github.com/Captainamarica/Night-Vission-Music >.
 #
 # This file is part of < https://github.com/TEAM-DARK-MUSIC/DarkMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
