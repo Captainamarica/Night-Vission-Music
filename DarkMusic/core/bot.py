6df98c17bbbf960df6f608c070ddf40e91d0a620
Navanjana <104069319+Captainamarica@users.noreply.github.com>
2022-05-22 14:05:41 +0530

    Update bot.py

--- DarkMusic/core/bot.py
+++ DarkMusic/core/bot.py
@@ -1,7 +1,7 @@
 #
-# Copyright (C) 2022 by TEAM-DARK-MUSIC@Github, < https://github.com/TEAM-DARK-MUSIC >.
+# Copyright (C) 2022 by TEAM-DARK-MUSIC@Github, < https://github.com/Captainamarica/Night-Vission-Music >.
 #
-# This file is part of < https://github.com/TEAM-DARK-MUSIC/DarkMusicBot > project,
+# This file is part of < https://github.com/Captainamarica/Night-Vission-Music > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TEAM-DARK-MUSIC/DarkMusicBot/blob/master/LICENSE >
 #
